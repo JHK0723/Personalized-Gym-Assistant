040b08bec09455e2e0c51c63e8ee74117c8226da
JHK0723 <jhk.300506@gmail.com>
2026-02-06 08:38:14 +0530

    indent

--- admin/admin-login.js
+++ admin/admin-login.js
@@ -81,7 +81,8 @@ function createAdminDashboard() {
     const announcementsList = container.querySelector('#announcementsList');
     if (announcements.length === 0) {
         announcementsList.innerHTML = '<p class="no-data">No announcements yet. Create one!</p>';
-    } else {
+    } 
+    else {
         announcements.slice(-5).reverse().forEach(announcement => {
             const announcementCard = document.createElement('div');
             announcementCard.className = 'announcement-item card';
